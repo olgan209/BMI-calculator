5f535396b3738fa02dc7baa1b09c552d99bdeafa
Olga <olganeboga209@gmail.com>
2024-10-14 16:15:36 +0600

    calculation works correctly

--- src/main/java/org/example/bmi_calculator/HelloController.java
+++ src/main/java/org/example/bmi_calculator/HelloController.java
@@ -8,9 +8,17 @@ import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private TextField weightTextField;
+    private TextField kgTextField;
     @FXML
-    private TextField heightTextField;
+    private TextField mTextField;
+    @FXML
+    private TextField feetTextField;
+    @FXML
+    private TextField inchestTextField;
+    @FXML
+    private TextField poundsTextField;
+
+
     @FXML
     private Label welcomeText;
     @FXML
@@ -18,42 +26,39 @@ public class HelloController {
 
     @FXML
     protected void calculateEnUn(){
-        double weight = Double.parseDouble(weightTextField.getText());
-        double height = Double.parseDouble(heightTextField.getText());
-        double bmi = (weight / (height * height)) * 703;
+        double pounds = Double.parseDouble(poundsTextField.getText());
+        double feet = Double.parseDouble(feetTextField.getText());
+        double inches = Double.parseDouble(inchestTextField.getText());
+        double height = (feet * 12) +inches;
+        double bmi = (703 * pounds / (height * height));
         if(bmi < 18.5){
             welcomeText.setText("Underweight:\tless than 18.5");
         }
-        else if(bmi > 18.5 && bmi < 24.9) {
+        else if(bmi >= 18.5 && bmi < 24.9) {
             welcomeText.setText("Normal:\tbetween 18.5 and 24.9");
         }
-        else if(bmi > 24.9 && bmi < 29.9) {
+        else if(bmi >= 24.9 && bmi < 29.9) {
             welcomeText.setText("Overweight:\tbetween 25 and 29.9");
         }
-        else if(bmi > 30) {
+        else if(bmi >= 30) {
             welcomeText.setText("Obese:\t30 or greater");
         }
     }
 
     @FXML
-    protected void calculateMeUn(){
-        double weight = Double.parseDouble(weightTextField.getText());
-        double height = Double.parseDouble(heightTextField.getText());
-        double bmi = weight / (height * height);
+    protected void calculateMeUn() {
+        double kg = Double.parseDouble(kgTextField.getText());
+        double m = Double.parseDouble(mTextField.getText());
+        double bmi = kg / (m * m);
 
-        if(bmi < 18.5){
+        if (bmi < 18.5) {
             welcomeText.setText("Underweight:\tless than 18.5");
-        }
-        else if(bmi > 18.5 && bmi < 24.9) {
+        } else if (bmi >= 18.5 && bmi < 24.9) {
             welcomeText.setText("Normal:\tbetween 18.5 and 24.9");
-        }
-        else if(bmi > 24.9 && bmi < 29.9) {
+        } else if (bmi >= 25 && bmi < 29.9) {
             welcomeText.setText("Overweight:\tbetween 25 and 29.9");
-        }
-        else if(bmi > 30) {
+        } else if (bmi >= 30) {
             welcomeText.setText("Obese:\t30 or greater");
         }
     }
-
-
 }
